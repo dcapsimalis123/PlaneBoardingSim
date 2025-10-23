dfaac2f73dcb9b1a387ff6be54f200ab1b854e73
Donovan Capsimalis <donovan.capsimalis@gmail.com>
2025-10-22 23:33:05 -0700

    rename the executable and update to do list.

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ TODO:
 0. Need to swap the check to check for all people in front while not overloading the sim, with better organized boarding, current is works (thus current implimentation), but for chaotic seating orders, this needs to be addressed.
 1. Impliment sequencing for passengers
     a. Will want a general function call that reads a datastructure or sub function that sets the passengers in sequence
+    b. *** need to impliment back to front loading, to do so I need to fill the magic numbers with values (ie plane length, plane width, etc.)
 2. Change displaying to terminal so that instead it writes it all to a log file
 3. Stopping for Baggage time, I am thinking adding some sort of timer to the passengers
     a. so to have varying movement times, could do it instead as a count down from 10, and with varying speeds it deletes it until its zero, and once its zero, allow movement through baggage lane
